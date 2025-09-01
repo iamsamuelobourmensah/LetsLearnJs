3b2cb334a74c3a609a94f6e4dd435e8c1b2c67a0
Samuel Obour Mensah <msamuelobourmensah@gmail.com>
2025-09-01 17:56:37 +0000

    first commit

---
+++ mythirdweb.js
@@ -0,0 +1,46 @@
+//let userName = window.prompt("Please enter your name:"
+
+let userName ;
+
+// document.getElementById("Submit").onclick = function myFunction(){
+
+// userName = document.getElementById("userName").value;
+
+// console.log(userName);
+
+// document.getElementById("di").innerHTML = "Hello " + userName;
+
+// }
+
+
+// let userAge = window.prompt("Please how old are you?");
+// userAge = Number(userAge);
+// userAge+=1;
+// console.log(userAge);
+
+// Area of a rectangle
+
+let length;
+let width = 5;
+let area;
+length = Number(length);
+
+// document.getElementById("calculateArea").onclick = function calculateArea(){
+//     length = document.getElementById("length").value;
+// length = Number(length);
+
+// //     width = document.getElementById("width").value;
+// // width = Number(width);
+//     area = length * width;
+//     console.log(area);
+//     document.getElementById("areaResult").innerHTML = "Area: " + area;
+// }
+
+
+document.getElementById("calculateArea").onclick = function (){
+length = document.getElementById("legnhtInput").value;
+area = length * width;
+console.log(area);
+document.getElementById("resultArea").textContent = "Area: " + area;
+
+};
