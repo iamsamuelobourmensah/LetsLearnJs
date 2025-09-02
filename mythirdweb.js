aa0725ec2612b432e1cc3e0df84ff99a19deccc3
Samuel Obour Mensah <msamuelobourmensah@gmail.com>
2025-09-02 23:16:26 +0000

    counter done

--- mythirdweb.js
+++ mythirdweb.js
@@ -1,46 +1,68 @@
-//let userName = window.prompt("Please enter your name:"
+// //let userName = window.prompt("Please enter your name:"
 
-let userName ;
+// let userName ;
 
-// document.getElementById("Submit").onclick = function myFunction(){
+// // document.getElementById("Submit").onclick = function myFunction(){
 
-// userName = document.getElementById("userName").value;
+// // userName = document.getElementById("userName").value;
 
-// console.log(userName);
+// // console.log(userName);
 
-// document.getElementById("di").innerHTML = "Hello " + userName;
+// // document.getElementById("di").innerHTML = "Hello " + userName;
 
-// }
+// // }
 
 
-// let userAge = window.prompt("Please how old are you?");
-// userAge = Number(userAge);
-// userAge+=1;
-// console.log(userAge);
+// // let userAge = window.prompt("Please how old are you?");
+// // userAge = Number(userAge);
+// // userAge+=1;
+// // console.log(userAge);
 
-// Area of a rectangle
+// // Area of a rectangle
 
-let length;
-let width = 5;
-let area;
-length = Number(length);
-
-// document.getElementById("calculateArea").onclick = function calculateArea(){
-//     length = document.getElementById("length").value;
+// let length;
+// let width = 5;
+// let area;
 // length = Number(length);
 
-// //     width = document.getElementById("width").value;
-// // width = Number(width);
-//     area = length * width;
-//     console.log(area);
-//     document.getElementById("areaResult").innerHTML = "Area: " + area;
-// }
+// // document.getElementById("calculateArea").onclick = function calculateArea(){
+// //     length = document.getElementById("length").value;
+// // length = Number(length);
+
+// // //     width = document.getElementById("width").value;
+// // // width = Number(width);
+// //     area = length * width;
+// //     console.log(area);
+// //     document.getElementById("areaResult").innerHTML = "Area: " + area;
+// // }
 
 
-document.getElementById("calculateArea").onclick = function (){
-length = document.getElementById("legnhtInput").value;
-area = length * width;
-console.log(area);
-document.getElementById("resultArea").textContent = "Area: " + area;
+// document.getElementById("calculateArea").onclick = function (){
+// length = document.getElementById("legnhtInput").value;
+// area = length * width;
+// console.log(area);
+// document.getElementById("resultArea").textContent = "Area: " + area;
 
-};
+// };
+
+const counterLabel = document.getElementById("counterLabel")
+const decreaseButton = document.getElementById("decreaseButton");
+const resetButton = document.getElementById("resetButton");
+const increaseButton = document.getElementById("increaseButton");
+let count = 0;
+
+
+increaseButton.onclick = function(){
+    count++
+    counterLabel.innerText = count;
+}
+
+decreaseButton.onclick = function(){
+    count--
+    counterLabel.innerText = count;
+}
+
+resetButton.onclick = function(){
+    count = 0
+    counterLabel.innerText = count;
+}
